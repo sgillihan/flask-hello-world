96370cfaef2281a628355038e1b91969b5ac6409
Stephanie Gillihan <sgillihan@gmail.com>
2026-04-07 05:01:31 +0000

    db_create completed

--- app.py
+++ app.py
@@ -15,4 +15,18 @@ def testing():
     conn.close()
     return "Database Connection Successful"
 
-
+@app.route('/db_create')
+def creating():
+    conn = psycopg2.connect("postgresql://sgillihan_lab_10_db_user:kTTPWao4DcXhVIneAbWvw9VVlWhYAdwL@dpg-d7a8jbp5pdvs73c31dkg-a/sgillihan_lab_10_db")
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball (
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int);
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Created"
